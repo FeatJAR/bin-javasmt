50bca897d98e78d6bdbb33fbc5b320745729e033
Elias Kuiter <info@elias-kuiter.de>
2023-03-15 10:41:14 +0100

    Added Z3 SMT solver support (tested on Linux)

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Binaries downloaded from
 
 * https://repo.maven.apache.org/maven2/org/sosy-lab/javasmt-solver-mathsat5/5.6.8/
+* https://repo.maven.apache.org/maven2/org/sosy-lab/javasmt-solver-z3/4.12.1/
+
+todo: improve version handling, use maven artifacts instead of committing binaries?
--- src/main/java/de/featjar/bin/javasmt/JavaSMTBinary.java
+++ src/main/java/de/featjar/bin/javasmt/JavaSMTBinary.java
@@ -23,9 +23,11 @@ package de.featjar.bin.javasmt;
 import de.featjar.base.data.Sets;
 import de.featjar.base.env.ABinary;
 import de.featjar.base.env.HostEnvironment;
+
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.LinkedHashSet;
+
 import org.sosy_lab.common.NativeLibraries;
 
 public class JavaSMTBinary extends ABinary {
@@ -42,7 +44,9 @@ public class JavaSMTBinary extends ABinary {
     @Override
     public LinkedHashSet<String> getResourceNames() {
         return HostEnvironment.isWindows()
-                ? Sets.of("mpir.dll", "mathsat.dll", "mathsat5j.dll")
-                : Sets.of("libmathsat5j.so");
+                ? Sets.of("mpir.dll", "mathsat.dll", "mathsat5j.dll", "libz3.dll", "libz3java.dll")
+                : HostEnvironment.isMacOS()
+                ? Sets.of("libmathsat5j.so", "libz3.dylib", "libz3java.dylib")
+                : Sets.of("libmathsat5j.so", "libz3.so", "libz3java.so");
     }
 }
